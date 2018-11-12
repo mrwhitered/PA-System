c5984ba70680f4267c63c05016a41cee253def7a
Furkan Akal <30433306+mrwhitered@users.noreply.github.com>
2018-11-12 14:39:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("***** Welcome to the System *****");
+
+            String girdi;
+
+            Console.Write("Enter the app that you want to open it: ");
+            girdi = Console.ReadLine();
+            
+            if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else if (girdi == "") {
+
+                System.Diagnostics.Process.Start(@"");
+
+            }
+
+            else {
+
+                Console.Write("Hatalı girdi...");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
